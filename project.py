9c966dc90eb5ecbedff3fa5c2b82ebae0745adc8
OstapSavyskyi <ostap.v.savytskyi@ukd.edu.ua>
2024-04-09 22:33:28 +0300

    Classes

---
+++ project.py
@@ -0,0 +1,30 @@
+from datetime import datetime
+
+class Customer:
+    {
+
+    }
+
+class Appoitment:
+    {
+
+    }
+
+class Barber:
+    {
+
+    }
+
+class Schedule:
+    {
+
+    }
+
+def validate_datetime(date_str):
+    try:
+
+        return True
+    except ValueError:
+        return False
+dgkk
+
